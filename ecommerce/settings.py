fb49a2504aaf2675f7046fdcd5404aec0924dc15
25220001 <eslammonty79@gmail.com>
2025-06-23 03:04:28 +0300

    .

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -174,11 +174,11 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
   'default': {
   'ENGINE': 'django.db.backends.postgresql_psycopg2',
-  'NAME': 'my_db',
-  'USER' : 'hero',
-  'PASSWORD' : 'my_db@123',
-  'HOST' : 'localhost',
-  'PORT' : '5432',
+  'NAME': 'railway',
+  'USER' : 'postgres',
+  'PASSWORD' : 'KHNZTPNPtRGdYmuJuGFhyJtaqgZHEmsO',
+  'HOST' : 'gondola.proxy.rlwy.net',
+  'PORT' : '17279',
   }
 }
 
