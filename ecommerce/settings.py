2a5b420c984c9b6455ebaae0fcbbeb36cf2bb34e
A .M <ahmedzzabdalla0@gmail.com>
2025-06-25 00:02:15 +0300

    change: change the allowed hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zii+5lo(@ze6$(9=s)idbfjhck(v!kch)-t#u#9k)shy8!zom1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1',".now.sh"]
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1',".now.sh", ".funco.publicvm.com", "13.51.197.20"]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
