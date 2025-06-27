2226a267ba8bc0dc756536c438e966c706f67ee9
A .M <ahmedzzabdalla0@gmail.com>
2025-06-27 23:12:25 +0300

    .

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -105,6 +105,9 @@ CORS_ALLOWED_ORIGINS = [
     # Add your frontend domain here
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_HEADERS = [
