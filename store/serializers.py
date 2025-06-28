87b492197f252aaf4202a35e529770ecb526babb
A .M <ahmedzzabdalla0@gmail.com>
2025-06-28 06:14:38 +0300

    .

--- store/serializers.py
+++ store/serializers.py
@@ -108,6 +108,11 @@ class ProductDetailSerializer(serializers.ModelSerializer):
             return request.build_absolute_uri(image.image.url) if request else image.image.url
         return None
 
+    def get_images(self, obj):
+        images = obj.images.exclude(
+            image__iendswith='.glb').order_by('-is_primary')
+        return ProductImageSerializer(images, many=True, context=self.context).data
+
     def get_available_colors(self, obj):
         available_colors = obj.available_colors
         return Color(available_colors, many=True, context=self.context).data
